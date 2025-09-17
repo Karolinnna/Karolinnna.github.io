004f7f799157e3b492bd3e68f9f80c6f4b105afe
Karolinnna <karoleti2024@gmail.com>
2025-09-17 22:29:42 +0300

    1.2 code

---
+++ Scripts/script.js
@@ -0,0 +1,4 @@
+console.log("count of p: "  + document.getElementsByTagName("p").length);
+console.log("count of h2: " + document.getElementsByTagName("h2").length);
+console.log(getComputedStyle(document.body).backgroundColor);
+console.log(getComputedStyle(document.getElementsByTagName("h1")[0]).fontSize);
