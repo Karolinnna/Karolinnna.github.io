18fa31ec8d087bcb46842aae172cf68ed0c7bde6
Karolinnna <karoleti2024@gmail.com>
2025-10-05 18:43:41 +0300

    Task 2. September 25

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,18 +1,26 @@
-//1.2 code
-console.log("count of p: "  + document.getElementsByTagName("p").length);
-console.log("count of h2: " + document.getElementsByTagName("h2").length);
-console.log(getComputedStyle(document.body).backgroundColor);
-console.log(getComputedStyle(document.getElementsByTagName("h1")[0]).fontSize);
+// 1 Task
+setTimeout(() => {
+    let imagesUrl = [
+        "https://t2.genius.com/unsafe/258x258/https%3A%2F%2Fimages.genius.com%2F3befd831f47fbd6b1a8e8773b9d701f1.1000x1000x1.png",
+        "https://t2.genius.com/unsafe/258x258/https%3A%2F%2Fimages.genius.com%2F1c7b05c05a17035ec8c7c09cd034c087.1000x1000x1.png",
+        "https://images.genius.com/c41a2c2daf4a3412ff1ebb5af8b8e589.1000x1000x1.png",
+        "https://images.genius.com/0bc94e0dc49c7b8cb7257ce9fd7bdb1f.1000x1000x1.png",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/3/36/Remember_Monday_-_What_the_Hell_Just_Happened%3F_cover_art.jpg/250px-Remember_Monday_-_What_the_Hell_Just_Happened%3F_cover_art.jpg"
+    ];
+    
+    imagesUrl.forEach((url, i) => {
+        
+        setTimeout(() => {
+            
+            let span = document.getElementById('gallery'+i);
+            
+            if (span) {
+                let img = document.createElement("img");
+                img.src = url;
+                span.appendChild(img);
+            }
+            
+        }, i * 1000); 
+    });
 
-//1.3 code
-let elem = document.body.childNodes;
-elem.forEach(element => {
-    if (element instanceof HTMLElement) {
-        element.addEventListener("mouseenter", () => {
-            element.style.backgroundColor = "red";
-        });
-        element.addEventListener("mouseleave", () => {
-            element.style.backgroundColor = "";
-        });
-    }
-});
+}, 5000);
